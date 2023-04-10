f17888fe252880adebc9a8aab17ab419303d0bc8
Miroslav Ivanov <znaka5@abv.bg>
2023-04-10 19:03:36 +0300

    09

--- Arrays and Nested Arrays Exercise/09magicMatrices.js
+++ Arrays and Nested Arrays Exercise/09magicMatrices.js
@@ -31,8 +31,29 @@ function magicMatrices(matrix) {
 
     console.log(flag);
 
-} magicMatrices(
-    [[11, 32, 45],
-    [21, 0, 1],
-    [21, 1, 1]]
+} 
+
+function magicMatrices(matrix) {
+    let rowSum = 0;
+    let colSum = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        const row = matrix[i].join('');
+        for (let j = 0; j < row.length; j++) {
+            rowSum += Number(row[j]);
+            colSum += Number(row[0]);
+        }
+        
+    }
+    if(rowSum ==colSum){
+        return console.log('true');
+    }else{
+        return console.log('false');
+    }
+}
+magicMatrices(
+    [[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]
+   
 )
