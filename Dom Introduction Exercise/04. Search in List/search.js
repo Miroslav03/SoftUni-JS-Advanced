6fd1c7ef8e7ff82c4fd9d4e0f3a56961b97a934c
Miroslav Ivanov <znaka5@abv.bg>
2023-04-25 01:48:53 +0300

    03-05

--- Dom Introduction Exercise/04. Search in List/search.js
+++ Dom Introduction Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   const searchBox = document.querySelector('#searchText')
+   const list  = document.querySelectorAll('#towns li')
+   const townList  = Array.from(list)
+
+   let counter = 0;
+
+   for (let i = 0; i < townList.length; i++) {
+      const element = townList[i];
+      if(element.textContent.includes(searchBox.value)){
+         element.style.fontWeight = 'bold'
+         element.style.textDecoration = 'underline'
+         counter++
+      }     
+   }
+
+   document.querySelector('#result').textContent = `${counter} matches found` 
 }
--- Dom Introduction Exercise/05. Table - Search Engine/solution.js
+++ Dom Introduction Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   const search  = document.querySelector('#searchField')
+   const tableElements = document.querySelectorAll('tbody tr')
 
    function onClick() {
-      //   TODO:
-
+      const arr = Array.from(tableElements)
+      for (let i = 0; i < arr.length; i++) {
+         const row = tableElements[i];
+         if(row.textContent.toLowerCase().includes(search.value.toLowerCase().trim())){
+            row.classList.add(`select`)
+         }else{
+            row.classList.remove('select')
+         }
+      }
+      search.value = ''
    }
+
 }
