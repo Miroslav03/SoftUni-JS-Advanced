91d03ab90ce2c801d5f490cc8ff74699bb8b7dd5
Miroslav Ivanov <znaka5@abv.bg>
2023-04-26 14:14:41 +0300

    06

--- Dom Introduction Exercise/06. Format the Text/solution.js
+++ Dom Introduction Exercise/06. Format the Text/solution.js
@@ -1,7 +1,19 @@
 function solve() {
-  const input = document.getElementById('input')
+  const input = document.getElementById('input').value
   const output = document.getElementById('output')
 
-  
+  const text = input.split(".").filter(e=>e.length>0)
+
+    for (i=0;i<text.length;i+=3){
+        const result = [];
+        for(j=0;j<3;j++){
+            if (text[i+j]){
+              result.push(text[i+j])
+            }    
+        }
+        const res = result.join(". ")+"."
+        output.innerHTML += `<p>${res}</p>`
+        
+      }
 
 }
