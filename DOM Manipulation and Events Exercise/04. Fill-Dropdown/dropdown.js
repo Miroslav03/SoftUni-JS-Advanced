ca5d927f733e3bb60225384484a723337b37003a
Miroslav Ivanov <znaka5@abv.bg>
2023-05-02 13:18:56 +0300

    04

--- DOM Manipulation and Events Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulation and Events Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemText = document.getElementById('newItemText')
+    const newItemValue = document.getElementById('newItemValue')
+
+    const menu = document.getElementById('menu')
+    const text = document.createElement('option')
+
+    text.value = newItemValue.value
+    text.textContent = newItemText.value
+
+    menu.appendChild(text)
+
+    newItemText.value = ''
+    newItemValue.value = ''
+
 }
