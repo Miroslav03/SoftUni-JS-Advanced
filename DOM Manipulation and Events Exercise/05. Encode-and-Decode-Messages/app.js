a85134e5fe57955426f318d3e1900c580c189d08
Miroslav Ivanov <znaka5@abv.bg>
2023-05-04 01:27:16 +0300

    05 06

--- DOM Manipulation and Events Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation and Events Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,40 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttons  = document.querySelectorAll('#main div button')
+    const textBoxes = document.querySelectorAll('#main div textarea')
+    //firstTextArea
+    const buttonEncode  = buttons[0]
+    const enocdeText  = textBoxes[0]
+
+    buttonEncode.addEventListener('click',encode)
+    function encode(){
+        const text = enocdeText.value
+        let encodedText = ''
+
+        for (let i = 0; i < text.length; i++) {
+            const letter = text[i];
+            let asciiCode = letter.charCodeAt()
+            let newLetter  = String.fromCharCode(asciiCode+1)
+            encodedText += newLetter
+        }
+        enocdeText.value = ''
+        decodeText.value = encodedText
+
+    }
+    //secondTextArea
+    const buttonDecode = buttons[1]
+    const decodeText  = textBoxes[1]
+
+    buttonDecode.addEventListener('click',decode)
+    function decode(){
+        const text = decodeText.value
+        let decodededText = ''
+        for (let i = 0; i < text.length; i++) {
+            const letter = text[i];
+            let asciiCode = letter.charCodeAt()
+            let newLetter  = String.fromCharCode(asciiCode-1)
+            decodededText += newLetter
+        }
+        decodeText.value = decodededText
+    }
+
 }
--- DOM Manipulation and Events Exercise/06. Furniture/solution.js
+++ DOM Manipulation and Events Exercise/06. Furniture/solution.js
@@ -1,4 +1,90 @@
 function solve() {
 
-  //TODO...
+  //generateBox
+  //buyBox
+  const [generateBox, buyBox] = Array.from(document.querySelectorAll('textarea'));
+  //generateButton
+  //buyButton
+  const [generateButton, buyButton] = Array.from(document.querySelectorAll('button'));
+  //table
+  const table = document.querySelector('table');
+
+  generateButton.addEventListener('click', generate);
+
+  function generate() {
+    const inputObj = JSON.parse(generateBox.value);
+
+    for (const obj of inputObj) {
+      const tbody = document.querySelector('tbody');
+      const tr = document.createElement('tr');
+      //image
+      const tdImage = document.createElement('td');
+      const img = document.createElement('img');
+      img.src = obj.img;
+      tdImage.appendChild(img);
+      tr.appendChild(tdImage);
+      //name
+      const tdName = document.createElement('td');
+      const name = document.createElement('p');
+      name.textContent = obj.name;
+      tdName.appendChild(name);
+      tr.appendChild(tdName);
+      //price
+      const tdPrice = document.createElement('td');
+      const price = document.createElement('p');
+      price.textContent = Number(obj.price);
+      tdPrice.appendChild(price);
+      tr.appendChild(tdPrice);
+      //decoration
+      const tdDecoration = document.createElement('td');
+      const decoration = document.createElement('p');
+      decoration.textContent = Number(obj.decFactor);
+      tdDecoration.appendChild(decoration);
+      tr.appendChild(tdDecoration);
+      //checkBox
+      const tdCheckBox = document.createElement('td');
+      const checkBox = document.createElement('input');
+      checkBox.type = 'checkbox';
+      tdCheckBox.appendChild(checkBox);
+      tr.appendChild(tdCheckBox);
+
+      tbody.appendChild(tr);
+    }
+  }
+
+  const allNames = [];
+  const allPrice = [];
+  const allDecoration = [];
+
+  buyButton.addEventListener('click', buy);
+
+  function buy() {
+    const checkedBoxes = Array.from(document.querySelectorAll('tbody input')).filter(x => x.checked);
+
+    for (const box of checkedBoxes) {
+      const parent = box.parentElement.parentElement;
+      const data = Array.from(parent.children);
+
+      const name = data[1].textContent;
+      const price = Number(data[2].textContent);
+      const decoration = Number(data[3].textContent);
+
+      allPrice.push(price);
+      allNames.push(name);
+      allDecoration.push(decoration);
+    }
+    let totalPrice = 0;
+    for (const num of allPrice) {
+      totalPrice+=num;
+    }
+    let averageDec = 0
+    for (const dec of allDecoration) {
+      averageDec+=dec
+    } 
+    averageDec = averageDec/allDecoration.length
+
+    buyBox.textContent += `Bought furniture: ${allNames.join(', ')}\n`;
+    buyBox.textContent += `Total price: ${totalPrice.toFixed(2)}\n`
+    buyBox.textContent += `Average decoration factor: ${averageDec}`
+  }
 }
