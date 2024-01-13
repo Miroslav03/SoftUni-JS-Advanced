e1eedaa5aba91bd3a9b382350c0dca4bed6322c7
Miroslav Ivanov <znaka5@abv.bg>
2024-01-13 02:49:07 +0200

    Update demo2.js

--- demo2.js
+++ demo2.js
@@ -1,19 +1,13 @@
-const obj  = {
-    name:'Miro',
-    age:20,
-    sex:'male',
-    eyeColor:'Brown'
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    console.log(subtotalArray);
 }
 
-Object.defineProperty(obj,'name',{
-    value:'Miro',
-    enumerable:false,
-    writable:true,
-    configurable:false
-})
-
-for (const key in obj) {
-    console.log(key);
-}
-
-console.log(obj);
+subtotals([1,2,6])
